3e701de2418dd12071af46c4c6ee5065871a9695
mayday <maydaythecoder@gmail.com>
2025-08-11 18:43:21 -0400

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import React from 'react'
 import Card from './Components/card'
-
+import Navbar from './Components/Sections/Navbar'
 export default function Home() {
   return (
     <div>
-      <h1>Navbar</h1>
+      <Navbar />
       <h1>main content</h1>
       <h1>footer</h1>
       <Card />
