26ad040836c7a12f903efff5d3e1908b31289bd1
mayday <maydaythecoder@gmail.com>
2025-08-11 18:19:32 -0400

    initial commit

---
+++ app/Components/card.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className="bg-green-500 rounded-lg shadow-md p-4">
+      <h1>Card</h1>
+    </div>
+    
+  )
+}
